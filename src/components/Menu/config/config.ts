3eb32d7ebf467e450a39eec7d7b15eee8576e93e
lion cho <chowin21@gmail.com>
2021-10-15 20:47:04 +0900

    fix(menu): Update header menu

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -5,6 +5,13 @@ export type ConfigMenuItemsType = MenuItemsType & { hideSubNav?: boolean }
 
 const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
   {
+    label: "Home",
+    href: "/",
+    icon: "Home",
+    showItemsOnMobile: false,
+    items: [],
+  },
+  {
     label: t('Dashboard'),
     icon: 'Dashboard',
     href: '/dashboard',
@@ -12,103 +19,34 @@ const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
     items: [],
   },
   {
-    label: t('Trade'),
-    icon: 'Swap',
-    href: '/swap',
-    showItemsOnMobile: false,
-    items: [
-      {
-        label: t('Exchange'),
-        href: '/swap',
-      },
-      {
-        label: t('Liquidity'),
-        href: '/liquidity',
-      },
-    ],
+    label: "Trade",
+    href: "/swap",
+    icon: "Swap",
+    items: [],
   },
   {
-    label: t('Earn'),
-    href: '/farms',
-    icon: 'Earn',
-    items: [
-      {
-        label: t('Farms'),
-        href: '/farms',
-      },
-      {
-        label: t('Pools'),
-        href: '/pools',
-      },
-    ],
+    label: "Farms",
+    href: "/farms",
+    icon: "Farms",
+    items: [],
   },
   {
-    label: t('Win'),
-    href: '/prediction',
-    icon: 'Trophy',
-    items: [
-      {
-        label: t('Prediction (BETA)'),
-        href: '/prediction',
-      },
-      {
-        label: t('Lottery'),
-        href: '/lottery',
-      },
-    ],
+    label: "Pools",
+    href: "/pools",
+    icon: "Pools",
+    items: [],
   },
   {
-    label: t('NFT'),
-    href: '/collectibles',
-    icon: 'Nft',
-    showOnMobile: false,
-    showItemsOnMobile: false,
-    items: [
-      {
-        label: t('Collectibles'),
-        href: '/collectibles',
-      },
-    ],
+    label: "IDO",
+    href: "#",
+    icon: "ifo",
+    items: [],
   },
   {
-    label: '',
-    href: '/info',
-    icon: 'More',
-    hideSubNav: true,
-    items: [
-      {
-        label: t('Info'),
-        href: '/info',
-      },
-      {
-        label: t('IFO'),
-        href: '/ifo',
-      },
-      {
-        label: t('Voting'),
-        href: '/voting',
-      },
-      {
-        type: DropdownMenuItemType.DIVIDER,
-      },
-      {
-        label: t('Leaderboard'),
-        href: '/teams',
-      },
-      {
-        type: DropdownMenuItemType.DIVIDER,
-      },
-      {
-        label: t('Blog'),
-        href: 'https://pancakeswap.medium.com',
-        type: DropdownMenuItemType.EXTERNAL_LINK,
-      },
-      {
-        label: t('Docs'),
-        href: 'https://docs.pancakeswap.finance',
-        type: DropdownMenuItemType.EXTERNAL_LINK,
-      },
-    ],
+    label: "NFA",
+    href: "/collectibles",
+    icon: "collectibles",
+    items: [],
   },
 ]
 
